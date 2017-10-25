6668007d3e776f8af691a54648efff192b17b5cb
Ramiz Rahman <ramiz.rahman@outlook.com>
2017-10-26 01:53:03 +0600

    Create app structure with basic functionality

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard_to_guess_string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin ramiz732@gmail.com'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    'mysql+pymysql://ramiz:' + os.environ.get('DBPASS') +'@localhost/flask_app'
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+    'mysql+pymysql://ramiz:' + os.environ.get('DBPASS') +'@localhost/flask_app_test'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'mysql+pymysql://ramiz:' + os.environ.get('DBPASS') +'@localhost/flask_app'
+
+config = {
+    'development': DevelopmentConfig,
+    'testing':     TestingConfig,
+    'production':  ProductionConfig,
+    
+    'default':     DevelopmentConfig
+}
+
