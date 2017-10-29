d83911ee68dc61dbe551d7c0de9093dcd6d2c66f
Ramiz Rahman <ramiz.rahman@outlook.com>
2017-10-29 15:41:55 +0600

    Add user profiles

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for, current_app, abort
 from . import main
-from .. import db
+from .. import db, moment
 from ..models import User
 from ..email import send_email
 
@@ -10,3 +10,9 @@ from ..email import send_email
 def index():
     return render_template('index.html')
     
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
+
 
 
 class Permission:
@@ -58,6 +60,14 @@ class User(UserMixin, db.Model):
 
     # Relationship
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    # User Information
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -140,6 +150,11 @@ class User(UserMixin, db.Model):
 
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+        db.session.commit()
 
     def __repr__(self):
         return '<User %r>' % self.username
