6496ce360eb7f53a063e3264dab115c59684dd87
Ramiz Rahman <ramiz.rahman@outlook.com>
2017-11-22 19:58:55 +0600

    Add test coverage

--- manage.py
+++ manage.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 import os
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 from app import create_app, db
 from app.models import User, Follow, Role, Permission, Post, Comment
 from flask_script import Manager, Shell
@@ -18,13 +25,25 @@ manager.add_command('db', MigrateCommand)
 
 # Unit test launcher command
 @manager.command
-def test(coverage=False, test_name=None):
+def test(coverage=False):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
-    if test_name is None:
-        tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 
 if __name__ == '__main__':
